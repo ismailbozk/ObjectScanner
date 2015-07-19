2a5b540d1d891608012d8e8a04ef8fa7fdcffe66
Ismail Bozkurt <Ismail.Bozkurt@monitise.com>
2015-07-19 16:36:49 +0300

    csv image files were replaced with the png files.

--- ObjectScanner/ObjectScanner/Utilities/OSCameraFrameProvider.m
+++ ObjectScanner/ObjectScanner/Utilities/OSCameraFrameProvider.m
@@ -56,22 +56,6 @@ static int const kOSCameraFrameProviderHeight = 480;
     });
 }
 
-- (void)startSimulatingFrameCaptures
-{
-//    if ([self.delegate respondsToSelector:@selector(didCapturedImage:)])
-//    {
-//        dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0), ^{
-//            for (UIImage *img in self.images)
-//            {
-//                dispatch_async(dispatch_get_main_queue(), ^{
-//                    [self.delegate didCapturedImage:img];
-//                });
-//                [NSThread sleepForTimeInterval:16.6 / 1000.0];//16.6ms sleep
-//            }
-//        });
-//    }
-}
-
 #pragma mark - Utilities
 
 + (NSString *)stringForFile:(NSString *)fileName extension:(NSString *)extension
@@ -85,84 +69,73 @@ static int const kOSCameraFrameProviderHeight = 480;
     return fileString;
 }
 
-//+ (float *)depthForFilePrefix:(NSString *)filePrefix
-//{
-//    NSString *resourceFileName = [NSString stringWithFormat:@"%@Depth", filePrefix];
-//    NSString *fileString = [self stringForFile:resourceFileName extension:@"csv"];
++ (UIImage *)imageForFilePrefix:(NSString *)filePrefix
+{
+    NSString *imageName = filePrefix;
+    UIImage *image = [UIImage imageNamed:imageName];
+    return image;
+    
+    //if you want to read images from csv file uncomment below.
+//    NSString *resourceFileName = [NSString stringWithFormat:@"%@", filePrefix];
+//    NSString *fileString = [self stringForFile:resourceFileName extension:@"png"];
 //    if (!fileString) {
 //        NSLog(@"Error reading file.");
 //    }
-//    NSArray *depthValues = [fileString componentsSeparatedByCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"\n;"]];
 //    
-////    float *depths = 
+//    NSArray *channels = [fileString componentsSeparatedByCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"\n;"]];
+//    
+//    unsigned c = (unsigned)(kOSCameraFrameProviderWidth*kOSCameraFrameProviderHeight * 4);//file only contains r,g and b data alpha is always 1
+//    uint8_t *bytes = malloc(sizeof(*bytes) * c);//
+//    
+//    unsigned i;
+//    for (i = 0; i < kOSCameraFrameProviderWidth*kOSCameraFrameProviderHeight; i++)
+//    {
+//        unsigned ind = 4*i;
+//        unsigned dataInd = 3*i;
+//        //r
+//        NSString *str = [channels objectAtIndex:dataInd];
+//        int byte = [str intValue];
+//        bytes[ind + 2] = (uint8_t)byte;
+//        
+//        //g
+//        str = [channels objectAtIndex:dataInd+1];
+//        byte = [str intValue];
+//        bytes[ind+1] = (uint8_t)byte;
+//        
+//        //b
+//        str = [channels objectAtIndex:dataInd+2];
+//        byte = [str intValue];
+//        bytes[ind] = (uint8_t)byte;
+//        
+//        //a
+//        bytes[ind+3] = (uint8_t)255;
+//    }
+//    
+//    CGDataProviderRef provider = CGDataProviderCreateWithData(NULL, bytes, c * 4, NULL);
 //
+//    int bitsPerComponent = 8;
+//    int bitsPerPixel = 32;
+//    int bytesPerRow = 4*kOSCameraFrameProviderWidth;
+//    CGColorSpaceRef colorSpaceRef = CGColorSpaceCreateDeviceRGB();
+//    CGBitmapInfo bitmapInfo = kCGBitmapByteOrderDefault |kCGImageAlphaLast;
+//    CGColorRenderingIntent renderingIntent = kCGRenderingIntentDefault;
+//    CGImageRef imageRef = CGImageCreate(kOSCameraFrameProviderWidth,
+//                                        kOSCameraFrameProviderHeight,
+//                                        bitsPerComponent,
+//                                        bitsPerPixel,
+//                                        bytesPerRow,
+//                                        colorSpaceRef,
+//                                        bitmapInfo,
+//                                        provider,
+//                                        NULL,
+//                                        NO,
+//                                        renderingIntent);
 //    
-//    
-//    return nil;
-//}
-
-+ (UIImage *)imageForFilePrefix:(NSString *)filePrefix
-{
-    NSString *resourceFileName = [NSString stringWithFormat:@"%@RGB", filePrefix];
-    NSString *fileString = [self stringForFile:resourceFileName extension:@"csv"];
-    if (!fileString) {
-        NSLog(@"Error reading file.");
-    }
-    
-    NSArray *channels = [fileString componentsSeparatedByCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"\n;"]];
-    
-    unsigned c = (unsigned)(kOSCameraFrameProviderWidth*kOSCameraFrameProviderHeight * 4);//file only contains r,g and b data alpha is always 1
-    uint8_t *bytes = malloc(sizeof(*bytes) * c);//
-    
-    unsigned i;
-    for (i = 0; i < kOSCameraFrameProviderWidth*kOSCameraFrameProviderHeight; i++)
-    {
-        unsigned ind = 4*i;
-        unsigned dataInd = 3*i;
-        //r
-        NSString *str = [channels objectAtIndex:dataInd];
-        int byte = [str intValue];
-        bytes[ind + 2] = (uint8_t)byte;
-        
-        //g
-        str = [channels objectAtIndex:dataInd+1];
-        byte = [str intValue];
-        bytes[ind+1] = (uint8_t)byte;
-        
-        //b
-        str = [channels objectAtIndex:dataInd+2];
-        byte = [str intValue];
-        bytes[ind] = (uint8_t)byte;
-        
-        //a
-        bytes[ind+3] = (uint8_t)255;
-    }
-    
-    CGDataProviderRef provider = CGDataProviderCreateWithData(NULL, bytes, c * 4, NULL);
-
-    int bitsPerComponent = 8;
-    int bitsPerPixel = 32;
-    int bytesPerRow = 4*kOSCameraFrameProviderWidth;
-    CGColorSpaceRef colorSpaceRef = CGColorSpaceCreateDeviceRGB();
-    CGBitmapInfo bitmapInfo = kCGBitmapByteOrderDefault |kCGImageAlphaLast;
-    CGColorRenderingIntent renderingIntent = kCGRenderingIntentDefault;
-    CGImageRef imageRef = CGImageCreate(kOSCameraFrameProviderWidth,
-                                        kOSCameraFrameProviderHeight,
-                                        bitsPerComponent,
-                                        bitsPerPixel,
-                                        bytesPerRow,
-                                        colorSpaceRef,
-                                        bitmapInfo,
-                                        provider,
-                                        NULL,
-                                        NO,
-                                        renderingIntent);
-    
-    /*I get the current dimensions displayed here */
-    UIImage *newImage = [UIImage imageWithCGImage:imageRef];
-    CGDataProviderRelease(provider);
-    CGColorSpaceRelease(colorSpaceRef);
-    return newImage;
+//    /*I get the current dimensions displayed here */
+//    UIImage *newImage = [UIImage imageWithCGImage:imageRef];
+//    CGDataProviderRelease(provider);
+//    CGColorSpaceRelease(colorSpaceRef);
+//    return newImage;
 }
 
 @end
